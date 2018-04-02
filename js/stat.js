520a8cffe3875d98c5a4e8db0d3fdfafdb3059e1
aretmii <geolog197@mail.ru>
2018-04-03 00:12:20 +0300

    mod2 first

---
+++ js/stat.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillStyle = 'white';
+  ctx.fillRect(100, 10, 420, 270);
+
+  ctx.font = '16px PT Mono';
+  ctx.strokeText('Ура вы победили!', 225, 30);
+  ctx.strokeText('Список результатов:', 210, 50);
+
+  for (var i = 0; i < )
+}
